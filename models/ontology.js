51dbf9fe0aeaf56b3cabb70db2f911c2a390a04e
lmedini <lionel.medini@univ-lyon1.fr>
2015-11-21 18:03:37 +0100

    Test in models/ontology

--- models/ontology.js
+++ models/ontology.js
@@ -99,7 +99,6 @@ var ontology = {
         for (var i in composedFunctionalities) {
             if (!composedFunctionalitiesInfo[((composedFunctionalities[i]).subject)]) {
                 composedFunctionalitiesInfo[((composedFunctionalities[i]).subject)] = {
-                    "id":((composedFunctionalities[i]).subject),
                     '@id': ((composedFunctionalities[i]).subject),
                     '@type': 'vocab:Functionality',
                     '@context': Globals.vocabularies.base + "/context/Functionality",
