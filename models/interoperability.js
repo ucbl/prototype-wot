d0134fdb3793c03c270f4dd6b9ee66ef027eee66
lmedini <lionel.medini@univ-lyon1.fr>
2015-11-25 11:35:02 +0100

    Try with interoperability layer complete

--- models/interoperability.js
+++ models/interoperability.js
@@ -35,7 +35,7 @@
                         }
                     }
                     var tempObject = {
-                        '@id': Globals.vocabularies.capability + dataJson.id,
+                        '@id': Globals.vocabularies.appliance + dataJson.id,
                         '@context': Globals.vocabularies.interoperability + 'context/CimaObject',
                         '@type': 'vocab:CimaObject',
                         'id': dataJson.id,
