05f28fcef761af948eda9cb80444a96a507f5c08
lmedini <lionel.medini@univ-lyon1.fr>
2015-11-25 09:50:49 +0100

    Try with interoperability layer complete

--- controllers/test.js
+++ controllers/test.js
@@ -1,15 +1,9 @@
 //This file contains a mock of an interoperability platform that is supposed to provide access to the connected objects
 
 var express = require('express'),
-    router = express.Router(),
-    Globals = require('../models/globals');
+    router = express.Router();
 
 // Entry point and home page
 router.get('/', function(request, response) {
-        response.writeHead(200, {
-            "Content-Type": "application/ld+json",
-            "Link": Globals.vocabularies.linkVocab
-        });
-
         response.end("test");
 });
