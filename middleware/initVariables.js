c162df9bf74cb80c5ffce83b05aed1d8ca2f24f5
lmedini <lionel.medini@univ-lyon1.fr>
2015-11-28 18:30:44 +0100

    Try with interoperability layer complete

--- middleware/initVariables.js
+++ middleware/initVariables.js
@@ -4,12 +4,15 @@
  */
 
 (function(module) {
-    var Globals = require('../models/globals');
+    var Globals = require('../models/globals'),
+        interoperabilityModel = require('../models/interoperability'),
+        ontologyModel = require('../models/ontology');
+
 
     module.exports = function (request, response, next) {
         if(!Globals.baseUriUpdated) {
             //Set up the server URI in the global variables
-            Globals.vocabularies.updateBaseUri('http://' + req.hostname + (Globals.config.port !== 80 ? (':' + Globals.config.port) : '') + '/');
+            Globals.vocabularies.updateBaseUri('http://' + request.hostname + (Globals.config.port !== 80 ? (':' + Globals.config.port) : '') + '/');
 
             //Initiate the object discovery and construct their URIs
             ontologyModel.loadOntology({verbose: false});
