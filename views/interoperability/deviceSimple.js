a7fca524d628fb2467f146c2c3815aada753c410
lmedini <lionel.medini@univ-lyon1.fr>
2015-12-09 17:29:16 +0100

    Try with interoperability simple vocabulary

---
+++ views/interoperability/deviceSimple.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Lionel on 25/11/2015.
+ */
+//View returning a JSON-LD description of an device
+
+(function(module) {
+        var Globals = require('./globals');
+
+        module.exports = function (deviceModel) {
+        return {
+            '@id': deviceModel['@id'],
+            'name': deviceModel.name,
+            'description': deviceModel.description
+        }
+    };
+})(module);
