845c4139f10508eab34c90cb3de1885241a83fa9
lmedini <lionel.medini@univ-lyon1.fr>
2015-12-09 23:55:34 +0100

    Try with interoperability simple vocabulary

--- data/interoperability/capabilities/call.js
+++ data/interoperability/capabilities/call.js
@@ -26,7 +26,7 @@
         //Modifies a call if its 'id' and 'call' are provided in args
         //Adds a new call to the list if only 'call' is given
         "post": function (values, params) {
-            console.log(!isNaN("pfdihotu"));
+            console.log(!isNaN(params.id));
             console.log(parseInt(params.id) >= 0);
             console.log(parseInt(params.id) < calls.length);
             if(params & params['id'] && params['call'] && !isNaN(params.id) && parseInt(params.id) >= 0 && parseInt(params.id) < calls.length) {
