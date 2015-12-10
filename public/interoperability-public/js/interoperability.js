beb67fc7c755c13b59a039bfd7f9bf7735db230a
lmedini <lionel.medini@univ-lyon1.fr>
2015-12-10 13:01:30 +0100

    Try with interoperability simple vocabulary

--- public/interoperability-public/js/interoperability.js
+++ public/interoperability-public/js/interoperability.js
@@ -9,12 +9,6 @@ $(document).ready(function() {
         setInterval(function () {
             reloadConnectedDevices();
         }, 5000);
-        //Change links on devices into AJAX calls
-        $('div.deviceName a').click(function() {
-            alert("clic");
-            loadDevice(this.href);
-            return false;
-        });
 
 //Convenience function to test capability invocation with a JSON body in the request and a PUT method
 //Can be modified and re-sent using FF console...
