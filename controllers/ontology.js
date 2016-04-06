e9f6ef53fa27a458975584bebd03f8d92e4bc312
lmedini <lionel.medini@univ-lyon1.fr>
2016-04-07 00:08:03 +0200

    Interoperability controller redirects gateway requests

--- controllers/ontology.js
+++ controllers/ontology.js
@@ -46,7 +46,6 @@ router.get('/context', function(request, response, next) {
 router.get('/context/:context', jsonParser, function(request, response, next) {
     request.vocabUri = ontologyModel.getHydraVocabUri();
     jsonldHeaders(request, response, next);
-    console.log(JSON.stringify(request));
     response.end(JSON.stringify(ontologyModel.getContext(request.params.context)));
     return true;
 });
