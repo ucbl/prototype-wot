ff47bd35b680d0e1447351207b53985d9645e545
lmedini <lionel.medini@univ-lyon1.fr>
2016-04-11 13:25:10 -0400

    All vocabularies in /ontology

--- models/globals.js
+++ models/globals.js
@@ -50,7 +50,7 @@
             this.setServerUris();
             this.setOtherUris();
             exports.baseUriUpdated = true;
-            console.log("base URI updated: " + this.base);
+            //console.log("base URI updated: " + this.base);
         }
     };
     vocabularies.setServerUris();
