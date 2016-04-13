445ade80973d080d72d2cb1dc422feec38c564d8
lmedini <lionel.medini@univ-lyon1.fr>
2016-04-12 23:41:36 -0400

    Adding ASAWoO platform

--- data/ontology/ontologies/prefixes.js
+++ data/ontology/ontologies/prefixes.js
@@ -2,14 +2,13 @@
     var Globals = require("../../../models/globals");
     module.exports = {
         "@context": {
-            "asawooVocab": Globals.vocabularies.asawooVocab,
+            "asawooVocab": Globals.vocabularies.hydraVocab,
             "applicationCapability": Globals.vocabularies.capability,
             "applicationFunctionality": Globals.vocabularies.functionality,
             "owl": "http://www.w3.org/2002/07/owl#",
             "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
             "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-            "xsd": "http://www.w3.org/2001/XMLSchema#",
-            "prout": "prout"
+            "xsd": "http://www.w3.org/2001/XMLSchema#"
         }
     };
 })(module);
