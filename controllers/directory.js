d68e696ee4283d95ff8bc5a71f64951e5511aed6
lmedini <lionel.medini@univ-lyon1.fr>
2016-04-19 16:55:52 -0400

    Functionality directory in a separate router

--- controllers/directory.js
+++ controllers/directory.js
@@ -39,7 +39,7 @@ router.get('/:functionalityClassId', function(request, response, next) {
 
 router.put('/', jsonParser, function(request, response, next) {
     console.log("PUT: " + JSON.stringify(request.body));
-    var functionality = request.params["test"];
+    var functionality = request.body;
     request.vocabUri = asawooModel.getHydraVocabUri();
     jsonldHeaders(request, response, next);
     for (var i = 0; i < request.body.length; i++) {
